467152af18afe1c27c3526887a989981fb370b77
Michal Dygas <michal.dygas@shijigroup.com>
2021-06-17 23:04:10 +0200

    feat: add dicom sender

---
+++ sender/docker-entrypoint-local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o pipefail
+set -o errexit
+  
+log(){
+  MESSAGE=${1}
+  echo "[DICOM-SENDER] ${MESSAGE}"
+}
+
+dcmsend orthanc-server 4242 /dicoms/0002.DCM
+log "image has been sent"
